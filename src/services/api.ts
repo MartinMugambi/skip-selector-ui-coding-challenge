4770a9ecc5a923b165c2a0dd9f7b44f0db99b015
mmugambi <martinmwenda026@gmail.com>
2025-06-07 16:03:38 +0300

    add reusable api

---
+++ src/services/api.ts
@@ -0,0 +1,41 @@
+import API_BASE_URL from "../constants/contants";
+import SuccessResponseHandler from "./SuccessResponseHandler";
+import ErrorResponseHandler from "./ErrorReponseHandler";
+
+type ApiResponse = SuccessResponseHandler | ErrorResponseHandler;
+
+const request = async <T>(url: string): Promise<ApiResponse> => {
+  try {
+    const response = await fetch(`${API_BASE_URL}${url}`);
+    if (!response.ok) {
+      const error = await response.json();
+
+      return new ErrorResponseHandler(
+        false,
+        response.status,
+        error,
+        "Failed Request"
+      );
+    }
+
+    const data = await response.json();
+
+    return new SuccessResponseHandler(
+      true,
+      response.status,
+      data,
+      "Data Fetched Successfully"
+    );
+  } catch (error) {
+    return new ErrorResponseHandler(
+      false,
+      500,
+      error?.message ?? error,
+      "Request Failed"
+    );
+  }
+};
+
+const getElements = (url: string) => request(url);
+
+export default getElements;
