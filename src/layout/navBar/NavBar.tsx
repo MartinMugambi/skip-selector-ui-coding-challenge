ee82c0f3dd557ca0b7692757877ac9180ba5e1f4
mmugambi <martinmwenda026@gmail.com>
2025-06-08 02:26:24 +0300

    add navbar component

---
+++ src/layout/navBar/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBarProps from "./NavBar.types";
+import styles from "./NavBar.module.css";
+const NavBar = (props: NavBarProps) => {
+  const { title } = props;
+
+  return (
+    <section className={styles.navBarContainer}>
+      <h1>{title}</h1>
+    </section>
+  );
+};
+
+export default NavBar;
