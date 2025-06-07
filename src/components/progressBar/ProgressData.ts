fc923710a0c3de5c2d7594e8ed51f7ae6d915616
mmugambi <martinmwenda026@gmail.com>
2025-06-07 18:56:04 +0300

    add progress data

---
+++ src/components/progressBar/ProgressData.ts
@@ -0,0 +1,29 @@
+import { ProgressData } from "./ProgressBar.types";
+const progressBarData: ProgressData[] = [
+  {
+    id: 1,
+    title: "Postcode",
+  },
+  {
+    id: 2,
+    title: "Waste Type",
+  },
+  {
+    id: 3,
+    title: "Select Skip",
+  },
+  {
+    id: 4,
+    title: "Permit Check",
+  },
+  {
+    id: 5,
+    title: "Choose Date",
+  },
+  {
+    id: 6,
+    title: "Choose Date",
+  },
+];
+
+export default progressBarData;
