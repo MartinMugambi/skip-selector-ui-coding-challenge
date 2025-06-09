352a50c601988557aa259cc6f1ba49188b2dd368
mmugambi <martinmwenda026@gmail.com>
2025-06-09 13:04:16 +0300

    fix failing test

--- src/utils/__test__/getDeviceType.spec.ts
+++ src/utils/__test__/getDeviceType.spec.ts
@@ -1,18 +1,21 @@
 import getDeviceType from "../getDeviceType";
 
 describe("getDeviceType", () => {
-  it("returns 'mobile' for widths less than 768", () => {
-    expect(getDeviceType(320)).toBe("mobile");
+  test("returns 'mobile' for width less than 768", () => {
+    expect(getDeviceType(0)).toBe("mobile");
+    expect(getDeviceType(500)).toBe("mobile");
     expect(getDeviceType(767)).toBe("mobile");
   });
 
-  it("returns 'tablet' for widths between 768 and 1023", () => {
+  test("returns 'tablet' for width between 768 and 1278 inclusive", () => {
     expect(getDeviceType(768)).toBe("tablet");
-    expect(getDeviceType(1023)).toBe("tablet");
+    expect(getDeviceType(1000)).toBe("tablet");
+    expect(getDeviceType(1278)).toBe("tablet");
   });
 
-  it("returns 'desktop' for widths 1024 and above", () => {
-    expect(getDeviceType(1024)).toBe("desktop");
-    expect(getDeviceType(1440)).toBe("desktop");
+  test("returns 'desktop' for width greater than or equal to 1279", () => {
+    expect(getDeviceType(1279)).toBe("desktop");
+    expect(getDeviceType(1280)).toBe("desktop");
+    expect(getDeviceType(1920)).toBe("desktop");
   });
 });
